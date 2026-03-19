f04491d5fff3831fcd06ac79fc2d7c017d4ba67e
muriithikennedy443-sudo <muriithikennedy443@gmail.com>
2026-03-19 12:20:19 +0300

    Task 7.1 completed

---
+++ main.js
@@ -0,0 +1,9 @@
+// Variable declaration
+let name = "Kennedy";
+let age = 25;
+const birthYear = 2008;
+
+// type of operator
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof true);
